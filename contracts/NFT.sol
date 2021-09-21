133132b23c97278f37c27594c0418f65f41a3076
iveltondequeiroz <iveltondequeirozgmail.com>
2021-09-21 18:11:44 -0400

    mumbai net  config

---
+++ contracts/NFT.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFT is ERC721URIStorage {
+
+}
