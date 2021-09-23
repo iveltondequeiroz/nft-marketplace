cbcd04895ffedbcb1c21cc876677498363e71d3b
iveltondequeiroz <iveltondequeirozgmail.com>
2021-09-23 12:47:13 -0400

    load nfts

--- pages/index.js
+++ pages/index.js
@@ -36,6 +36,7 @@ export default function Home() {
       }
       return item
     }))
+    console.log("items:", items);
     setNfts(items)
     setLoadingState('loaded')
   }
@@ -73,23 +74,20 @@ export default function Home() {
                 <img src={nft.image} />
                 <div className="p-4">
                   <p style={{height: '64px'}} className="text-2xl font-semibold">
-                    {nft.name}
+                  {nft.name}
                   </p>
-                  <div style={{height: '70px', overflow: 'hidden'}} >
+                  <div style={{height: '70px', overflow: 'hidden'}}>
                     <p className="text-gray-400">{nft.description}</p>
                   </div>
                 </div>
                 <div className="p-4 bg-black">
                   <p className="text-2xl mb-4 font-bold text-white">{nft.price} Matic</p>
-                  <button className="w-full bg-pink-500 text-white font-bold py-2 px-12 rounded"
-                  onClick={() => buyNft(nft)}>Buy</button>
-                  </div>
+                  <button className="w-full bg-pink-500 text-white font-bold py-2 px-12 rounded" onClick={() => buyNft(nft)}>Buy</button>  
                 </div>
               </div>
             ))
           }
         </div>
-
       </div>
     </div>
   )
